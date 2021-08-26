16ec9446dbf1015a7328e767a4e28eb9a9844b82
user_DE <16607615570@163.com>
2021-08-26 23:46:02 +0800

    确定看板动作

--- assets/Script/Kanban.js
+++ assets/Script/Kanban.js
@@ -27,20 +27,82 @@ cc.Class({
 
     // onLoad () {},
 
+
+    ctor() {
+        this.SPEAKMode = [3, 4, 6];
+        this.IDLEMode = [0, 1, 2];
+
+        this.cnt = 0;
+    },
     start() {
         var self = this;
         // this.getComponent(Live2dComponent).live2d.loadModel("Hiyori")
         this.getComponent(Live2dComponent).loopIdelMotion = false;
-        this.cnt = 0;
+        // this.cnt = 0;
         // cc.find("Canvas/button").on(cc.Node.EventType.MOUSE_DOWN, function () {
         //     self.
         // });
     },
 
-    button() {
-        // this.getComponent(Live2dComponent).live2d.getModel(0).startRandomMotion(Live2dDefine.MotionGroupIdle, Live2dDefine.PriorityIdle);
-        this.getComponent(Live2dComponent).live2d.getModel(0).startMotion(Live2dDefine.MotionGroupIdle, this.cnt++, this.cnt);
-    }
+    _getMotionMode(m) {
+        if (!m) {
+            console.error("_getMotionMode param m is null");
+            return;
+        }
+
+        var ran = Math.round(Math.random() * 2);
+        console.log(this.SPEAKMode[ran]);
+
+        if (m == "SPEAK") {
+            return this.SPEAKMode[ran];
+        }
+        else if (m == "IDLE") {
+            return this.IDLEMode[ran];
+        }
+
+    },
+
+    /**
+     * @description 未知错误的弥补手段
+     * @param {number} m 错误编号 1:live2d首次切换Motion不生效, 
+     * 
+     */
+    _unkownErrorFixer(m) {
+        if (m == 1) {
+
+        }
+    },
+
+    /**
+     * @description 切换人物动作
+     * @param m String,"SPEAK", "IDLE"
+     */
+    changeMotion(m) {
+
+        if (!m) {
+            console.error("changeMotion param m is null");
+            return;
+        }
+
+        if (this.cnt == 0) {
+            this.getComponent(Live2dComponent).live2d.getModel(0).startMotion(Live2dDefine.MotionGroupIdle,
+                this._getMotionMode(m), this.cnt++);
+        }
+
+        this.getComponent(Live2dComponent).live2d.getModel(0).startMotion(Live2dDefine.MotionGroupIdle,
+            this._getMotionMode(m), this.cnt++);
+
+    },
+
+
+
+    // button() {
+    //     // this.getComponent(Live2dComponent).live2d.getModel(0).startRandomMotion(Live2dDefine.MotionGroupIdle, Live2dDefine.PriorityIdle);
+    //     // this.getComponent(Live2dComponent).live2d.getModel(0).startMotion(Live2dDefine.MotionGroupIdle, this.cnt++, this.cnt);
+    //     // console.log(this.cnt - 1);
+
+    //     this.changeMotion("SPEAK");
+    // }
 
     // update (dt) {},
 });
